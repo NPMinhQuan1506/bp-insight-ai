a09fb8163d28a86f54e7799e94f080f866b60a61
MusLunar <npminhquan1506@gmail.com>
2025-08-09 23:15:10 +0700

    add test for func loadPic

--- tests/test_image_loader.py
+++ tests/test_image_loader.py
@@ -1,12 +1,27 @@
 import os
-from . import FOLDER_IMAGE_NAME
+from app import FOLDER_IMAGE_NAME
 from app.image_loader import ail_loadImgAsGrayscale
 from app.utils import sysUtil_getImageList
-
+from random import randrange
 def til_testLoadImage_Success():
     folderName = FOLDER_IMAGE_NAME
     images = sysUtil_getImageList(folderName)
     if not images:
         assert False, f"No image files found in {folderName}"
     
+    images = sysUtil_getImageList(folderName)
+    randomPath = images[random(len(images) - 1)]
+    grayScale = ail_loadImgAsGrayscale(randomPath)
+    
+    assert isinstance(grayScale, list)
+    assert len(grayScale) > 0
+    assert all(isinstance(row, list) for row in grayScale)
+    assert all(isinstance(pixel, int) for pixels in grayScale[0])
+    assert 0 <= grayscale[0][0] <= 255
+
+    
+def til_testLoadImage_Fail():
+    path = "Image doesn't exist"
+    grayScale = ail_loadImgAsGrayscale(path)
+    assert grayscale == [], "Should return empty list on error"
     
