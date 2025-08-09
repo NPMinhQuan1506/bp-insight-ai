3cf840ce5ebab691db2c6b23def27ecbda99975c
MusLunar <npminhquan1506@gmail.com>
2025-08-09 23:32:43 +0700

    Fix testcase file

--- tests/test_image_loader.py
+++ tests/test_image_loader.py
@@ -2,26 +2,26 @@ import os
 from app import FOLDER_IMAGE_NAME
 from app.image_loader import ail_loadImgAsGrayscale
 from app.utils import sysUtil_getImageList
-from random import randrange
-def til_testLoadImage_Success():
+# from random import randrange
+from random import choice
+def test_LoadImage_Success():
     folderName = FOLDER_IMAGE_NAME
     images = sysUtil_getImageList(folderName)
     if not images:
         assert False, f"No image files found in {folderName}"
     
-    images = sysUtil_getImageList(folderName)
-    randomPath = images[random(len(images) - 1)]
-    grayScale = ail_loadImgAsGrayscale(randomPath)
-    
+    randomPath = choice(images)
+    selectedPath = os.path.join(folderName, randomPath)
+    grayScale = ail_loadImgAsGrayscale(selectedPath)
     assert isinstance(grayScale, list)
     assert len(grayScale) > 0
     assert all(isinstance(row, list) for row in grayScale)
-    assert all(isinstance(pixel, int) for pixels in grayScale[0])
-    assert 0 <= grayscale[0][0] <= 255
+    assert all(isinstance(px, int) for px in grayScale[0])
+    assert 0 <= grayScale[0][0] <= 255
 
     
-def til_testLoadImage_Fail():
+def test_LoadImage_Fail():
     path = "Image doesn't exist"
     grayScale = ail_loadImgAsGrayscale(path)
-    assert grayscale == [], "Should return empty list on error"
+    assert grayScale == [], "Should return empty list on error"
     
